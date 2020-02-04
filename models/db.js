713ff33104c61343d2bdab4e88f26e108ed582ec
vivek <vivek.koyani@gmail.com>
2020-02-04 22:37:31 +0530

    mongodb connection suc

---
+++ models/db.js
@@ -0,0 +1,6 @@
+const mongoose = requrie('mongoose');
+
+mongoose.connect('mongodb url', { useNewUrlParser: true}, (err) => {
+    if(!err) {console.log('Mongodb Connection Succeeded.')}
+    else {console.log('Error in DB connection : ' + err)}
+});
